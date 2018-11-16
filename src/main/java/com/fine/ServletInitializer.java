1b29f8b0f9b75f8b273a061665f677ec0dadbfc7
Jong <fine@fineinsight.kr>
2018-11-16 10:46:41 +0900

    ddddddddd

---
+++ src/main/java/com/fine/ServletInitializer.java
@@ -0,0 +1,15 @@
+package com.fine;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import com.fine.webservice.Application;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+}
